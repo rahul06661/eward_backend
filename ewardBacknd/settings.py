1e6c9379ab1b842c8c9d7ded8bbe3abbec7b2bb5
rahul06661 <rahulsisupalan0@gmail.com>
2023-04-17 14:22:03 +0530

    apr15

--- ewardBacknd/settings.py
+++ ewardBacknd/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-linpf5hqf@^19qv7&*8%9d%k9^&&9acf!sx(4rt!0mgb5-36m%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['n.n.n.n']
 
 
 # Application definition
